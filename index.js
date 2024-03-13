5fb8bf2b30b397094f849642e40a353b44d7b648
FIKRIWILDANP31 <163328500+FIKRIWILDANP31@users.noreply.github.com>
2024-03-13 23:12:01 +0700

    pixxx index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'PIXXX BOT'; 
+const instagram = 'fikri_3106- kaelMk_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:fikri\n' // Nama kamu
+            + 'ORG:pixxx bot;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6288985656645:+62 889-8565-6645\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
